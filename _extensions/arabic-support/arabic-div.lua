e522e75f97871bae3ffe437b6955892cd20e6282
Ahmad Sheikh <asheikh@Ahmads-MacBook-Pro.local>
2024-11-17 18:32:22 +0500

    Added support for alternative font Amiri

--- _extensions/arabic-support/arabic-div.lua
+++ _extensions/arabic-support/arabic-div.lua
@@ -1,14 +1,27 @@
 -- Add attributes for Arabic text in a span
 function Div (el)
-  if el.classes:includes 'ar' then
+  if el.classes:includes 'ar' or el.classes:includes 'aralt' then
     text = pandoc.utils.stringify(el)
+    --if FORMAT:match 'latex' and el.classes:includes 'aralt' then
+    --  text = [[\altfamily ]] .. text
+    --end
     contents = {pandoc.Str(text)}
     if FORMAT:match 'latex' then
+      if el.classes:includes 'aralt' then
+        table.insert(
+          contents, 1,
+          pandoc.RawInline('latex', '\\altfamily ')
+        )
+      end
       -- no dir needed for babel and throws error if it sees dir attribute. was previously needed for polyglossia
       return pandoc.Div(contents, {class='otherlanguage', data_latex="{arabic}", lang='ar'})
     elseif FORMAT:match 'html' then
+      classval = 'reg-ar-text'
+      if el.classes:includes 'aralt' then
+        classval = 'alt-ar-text'
+      end
       -- dir needed for html otherwise punctuation gets messed up
-      return pandoc.Div(contents, {class='regartext', lang='ar', dir='rtl'})
+      return pandoc.Div(contents, {class=classval, lang='ar', dir='rtl'})
     end
   end
 end
--- _extensions/arabic-support/inline-arabic-span.lua
+++ _extensions/arabic-support/inline-arabic-span.lua
@@ -1,14 +1,26 @@
 -- Add attributes for Arabic text in a span
 function Span (el)
-  if el.classes:includes 'ar' then
+  if el.classes:includes 'ar' or el.classes:includes 'aralt' then
     text = pandoc.utils.stringify(el)
     contents = {pandoc.Str(text)}
     if FORMAT:match 'latex' then
+      if el.classes:includes 'aralt' then
+        --text = [[\altfamily ]] .. text
+        --contents = {pandoc.RawInline('latex', '\\altfamily')} .. contents.content
+        table.insert(
+          contents, 1,
+          pandoc.RawInline('latex', '\\altfamily ')
+        )
+      end
       -- no dir needed for babel and throws error if it sees dir attribute. was previously needed for polyglossia
-      return pandoc.Span(contents, {class='regartext', lang='ar'})
+      return pandoc.Span(contents, {lang='ar'})
     elseif FORMAT:match 'html' then
+      classval = 'reg-ar-text'
+      if el.classes:includes 'aralt' then
+        classval = 'alt-ar-text'
+      end
       -- dir needed for html otherwise punctuation gets messed up
-      return pandoc.Span(contents, {class='regartext', lang='ar', dir='rtl'})
+      return pandoc.Span(contents, {class=classval, lang='ar', dir='rtl'})
     end
   end
 end
